674b7d562a58544ffb5e5ff5a0f96198225b5018
XomekX <z_v_m_x@mail.ru>
2022-04-09 22:43:03 +0700

    connected redux and installed redux-saga

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import { store } from "./redux/store";
+import { Provider } from "react-redux";
 import "./scss/index.scss";
 import App from "./App";
 
 const app = (
    <BrowserRouter>
-      <App />
+      <Provider store={store}>
+         <App />
+      </Provider>
    </BrowserRouter>
 );
 
