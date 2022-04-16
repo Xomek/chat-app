eeb1012ffd8087f9b73faee4422a36f917a26f32
XomekX <z_v_m_x@mail.ru>
2022-04-16 21:34:08 +0700

    changed input password validation

--- src/yup/login.shema.ts
+++ src/yup/login.shema.ts
@@ -2,5 +2,5 @@ import * as yup from "yup";
 
 export const validatationLogin = yup.object({
    email: yup.string().email("Не верный email").required("Обязательное поле"),
-   password: yup.string().min(6, "Пароль слишком короткий").required("Обязательное поле"),
+   password: yup.string().min(8, "Пароль слишком короткий").required("Обязательное поле"),
 });
--- src/yup/registrations.shema.ts
+++ src/yup/registrations.shema.ts
@@ -2,7 +2,12 @@ import * as yup from "yup";
 
 export const validatationReg = yup.object({
    email: yup.string().email("Не верный email").required("Обязательное поле"),
-   password: yup.string().min(6, "Пароль слишком короткий").required("Обязательное поле"),
+   password: yup
+      .string()
+      .min(8, "Пароль слишком короткий")
+      .required("Обязательное поле")
+      .matches(RegExp("(.*[a-z].*)"), "Используйте маленькую букву")
+      .matches(RegExp("(.*[A-Z].*)"), "Используйте большую букву"),
    confirm: yup
       .string()
       .oneOf([yup.ref("password")], "Пароли не совпадают")
